7cf7920287abae3c2286065b5fca78607fcc7949
PuppetDD <2475630190@qq.com>
2019-04-04 20:39:36 +0800

    Nio C-C

---
+++ src/com/protocol/User.java
@@ -0,0 +1,23 @@
+package com.protocol;
+
+/**
+ * C-C
+ * com.protocol
+ *
+ * @author GOLD
+ * @date 2019/4/4
+ */
+public class User {
+
+    private String name;
+    private String ipaddress;
+    private int port;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getIpaddress() {
+        return ipaddress;
+    }
+}
